408b14874b01f992309373489b264d52a8a7ebf8
Chen Bo Wei <U11187021@mail2.mcut.edu.tw>
2026-01-25 13:50:04 +0800

    test 1

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+from os import system
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.twse.com.tw/zh/trading/historical/stock-day.html")
+    print(page.title())
+    system("pause")
+    browser.close()
+page.get_by_label()
