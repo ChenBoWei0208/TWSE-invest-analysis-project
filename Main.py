417b899c960bae9fa46d68711b42b0680ca2d89c
Chen Bo Wei <U11187021@mail2.mcut.edu.tw>
2026-02-17 22:07:55 +0800

    Q program (1.3)

--- Main.py
+++ Main.py
@@ -1,16 +1,24 @@
 from os import system
 
 from playwright.sync_api import sync_playwright
+stock_year = input("請輸入查詢年分 (例如 115): ")
+
+
+stock_id = input("請輸入股票代碼 (例如 2330): ")
+if not stock_id.strip():
+    print('錯誤，股票代碼不可為空!')
+    exit()
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://www.twse.com.tw/zh/trading/historical/stock-day.html")
     print(page.title())
-    stock_id=input("請輸入股票代碼 (例如 2330): ")
     page.get_by_label("股票代碼").fill(stock_id)
     page.click('button.search')
     page.wait_for_selector('table.main-list')
     rows = page.query_selector_all('table.main-list tr')
     system("pause")
     browser.close()
+
+page.query_selector('h3').text_content()
